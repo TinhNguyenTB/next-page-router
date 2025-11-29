c5ba0e01f5d8bd708561df816828fdeb234b3a65
TinhNguyenTB <tinh271103@gmail.com>
2025-11-29 17:57:15 +0700

    Linking and Navigating

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
-import { Geist, Geist_Mono } from 'next/font/google'
-
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-})
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Home() {
-	return <div>Hello world</div>
+	const router = useRouter()
+	const goToPostDetail = () => {
+		router.push({
+			pathname: '/categories/[categoryId]/posts/[postId]',
+			query: {
+				categoryId: 1,
+				postId: 2,
+			},
+		})
+	}
+	return (
+		<div>
+			<h5>Hello world</h5>
+			<Link href={'/blog'}>Go to blog</Link>
+			<button
+				className="border block p-2 rounded-md mt-2 hover:bg-amber-50 cursor-pointer transition-all"
+				onClick={goToPostDetail}
+			>
+				Go to post detail
+			</button>
+		</div>
+	)
 }
