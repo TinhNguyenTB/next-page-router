f91c1ce3eb4d86d4791ff3537345ee99e5178b8e
TinhNguyenTB <tinh271103@gmail.com>
2025-11-29 15:20:24 +0700

    setup prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from 'next/font/google'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
+})
 
 export default function Home() {
-  return <div>Hello world</div>;
+	return <div>Hello world</div>
 }
